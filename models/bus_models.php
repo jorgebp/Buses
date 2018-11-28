00c575d7ba6d2ec36276c1023528dada064c9204
jorgebp <jorge.beltranp@outlook.com>
2018-11-28 20:37:38 -0300

    Archivo modelo bus con todas sus función

--- models/bus_models.php
+++ models/bus_models.php
@@ -16,7 +16,7 @@
 
         //Constructor de la clase.
         function __construct($id, $nombreempresa, $maximoasiento, $largo, $ancho,
-                             $patente, $origen, $destino, $estaengarage, $reviciontecnica,
+                             $patente, $origen, $destino, $estaengarage, $revisiontecnica,
                              $comentario)
         {
             $this->id=$id;
@@ -42,11 +42,68 @@
             //Cargar en la $listaBuses cada registro desde la base de datos
             foreach($sql->fetchAll() as $buses)
             {
-                $listaBuses[]= new Buses($bues['id'],$bues['nombre'],$bues['asiente'],$bues['largo'],
-                                         $bues['ancho'],$bues['patente'],$bues['origen'],$bues['destino'],
-                                         $bues['garege'],$bues['revision'],$bues['comentario']);
+                $listaBuses[]= new Buses($buses['id'],$buses['nombreempresa'],$buses['maximoasiento'],$buses['largo'],
+                                         $buses['ancho'],$buses['patente'],$buses['origen'],$buses['destino'],
+                                         $buses['estaengarage'],$buses['revisiontecnica'],$buses['comentario']);
             }
             return $listaBuses;
+        }
+
+        //Función para registrar buses.
+        public static function save($buses)
+        {
+            $db=Db::getConnect();
+            $insert=$db-prepare('INSERT INTO BUSES VALUES(:id,:nombreempresa,:maximoasiento,:largo,
+                                                          :ancho,:patente,:origen,:destino,
+                                                          :estaengarage,:revisiontecnica,:comentario)');
+            $insert->bindValue('id',$buses->$id);
+            $insert->bindValue('nombre',$buses->$nombreempresa);
+            $insert->bindValue('asiento',$buses->$maximoasiento);
+            $insert->bindValue('largo',$buses->$largo);
+            $insert->bindValue('ancho',$buses->$ancho);
+            $insert->bindValue('patente',$buses->$patente);
+            $insert->bindValue('origen',$buses->$origen);
+            $insert->bindValue('destino',$buses->$destino);
+            $insert->bindValue('garage',$buses->$estaengarage);
+            $insert->bindValue('revision',$buses->$revisiontecnica);
+            $insert->bindValue('comentario',$buses->$comentario);
+            $insert->execute();
+        }
+
+        //Función para actualizar buses.
+        public static function update($buses)
+        {
+            $db=Db::getConnect();
+            $update=$db->prepare('UPDATE BUSES SET nombre=:nombreempresa, asiento=:maximoasiento, largo=:largo, 
+                                                   ancho=:ancho, patente=:patente, origen=:origen, destino=:destino, 
+                                                   garage=:estaengarage, revision=:revisiontecnica, comentario=:comentario WHERE id=:id');
+            $update->bindValue('id',$buses->$id);
+            $update->bindValue('nombre',$buses->$nombreempresa);
+            $update->bindValue('asiento',$buses->$maximoasiento);
+            $update->bindValue('largo',$buses->$largo);
+            $update->bindValue('ancho',$buses->$ancho);
+            $update->bindValue('patente',$buses->$patente);
+            $update->bindValue('origen',$buses->$origen);
+            $update->bindValue('destino',$buses->$destino);
+            $update->bindValue('garage',$buses->$estaengarage);
+            $update->bindValue('revision',$buses->$revisiontecnica);
+            $update->bindValue('comentario',$buses->$comentario);
+            $update->execute();
+        }
+
+        //Función para eliminar buses por el id.
+        public static function delete($id)
+        {
+            $db=Db::getConnect();
+            $delete=$db->prepare('DELETE FROM BUSES WHERE id=:id');
+            $delete->bindValue('id',$id);
+            $delete->execute();
+        }
+
+        //Funcion para dar de baja un bus por id.
+        public static function give_up()
+        {
+
         }
     }
 ?>
